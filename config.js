54571a5d914a0276783d968da5c7b69977ec4205
Ellyess Benmoufok <benmoufok.ellyess@gmail.com>
2026-02-18 20:00:02 +0000

    logo added

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.SUPABASE_URL = "https://mlsydruhwihfsfunretw.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sc3lkcnVod2loZnNmdW5yZXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NDA3MTUsImV4cCI6MjA4NzAxNjcxNX0.V7ogs3zLNdqMWJ5Q63qBkek-vz3bKbQM5G5JE4f132I";
 window.GUILD_PASSCODE = "3NT3R5W4X";
 window.GUILD_TITLE = "SWAX";
-window.GUILD_LOGO_URL = "./assets/logo.svg";
+window.GUILD_LOGO_URL = "./assets/logo.png";
 window.GUILD_BACKGROUND_URL = "./assets/background.svg";
